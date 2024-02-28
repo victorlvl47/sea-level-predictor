0ab42bf238a6a5ab88ab1b797cc8ff760235e67b
victor <victor.lvl47@gmail.com>
2024-02-27 19:58:03 -0600

    Create scatter plot

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -1,13 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
+import numpy as np
+
+called_colorbar = False
 
 def draw_plot():
     # Read data from file
     df = pd.read_csv('epa-sea-level.csv')
 
     # Create scatter plot
-
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'],
+                c=df['CSIRO Adjusted Sea Level'], cmap='viridis_r')
+    # FIX ME: Quick hack call colorbar() once.
+    global called_colorbar
+    if not called_colorbar:
+        plt.colorbar()
+        called_colorbar = True
 
     # Create first line of best fit
 
