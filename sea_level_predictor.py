d3af8986f3fb9c872f4bc8e88a99928899c85c51
victor <victor.lvl47@gmail.com>
2024-03-07 23:31:46 -0600

    refactor 1st best fitt line, add a second best fitt line

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -22,14 +22,24 @@ def draw_plot():
     # Create first line of best fit
     lin_out = linregress(x_years, y_sea_level)
     min_year = np.min(x_years)
+    max_year = np.max(x_years)
     proyected_year = 2050
-    x_fit = np.linspace(min_year, proyected_year)
+    num = len(x_years) + (proyected_year - max_year)
+    x_fit = np.linspace(min_year, proyected_year, num=num)
     y_fit = x_fit * lin_out[0] + lin_out[1]
 
     plt.plot(x_fit, y_fit, color="red", label='best fitted line to predict \nthe sea level rise in 2050.')
 
     # Create second line of best fit
+    x_years = x_years[120:]
+    y_sea_level = y_sea_level[120:]
+    lin_out = linregress(x_years, y_sea_level)
+    min_year = np.min(x_years)
+    num = len(x_years) + (proyected_year - max_year)
+    x_fit = np.linspace(min_year, proyected_year, num=num)
+    y_fit = x_fit * lin_out[0] + lin_out[1]
 
+    plt.plot(x_fit, y_fit, color="#d6ff00", label='predict the sea level rise in 2050 \nif the rate of rise continues as it has \nsince the year 2000')
 
     # Add labels and title
 
